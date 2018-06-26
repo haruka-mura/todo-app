2e5a9a92360652844e4f4076a4fd8c57e2806665
haruka-mura <muramoto.haruka@mwed.co.jp>
2018-06-26 15:35:10 +0900

    Modelのテスト追加

--- spec/models/task_due_date_spec.rb
+++ spec/models/task_due_date_spec.rb
@@ -4,8 +4,22 @@ RSpec.describe TaskDueDate, type: :model do
   describe '#valid?' do
     subject { build :task_due_date, attributes }
 
-    let(:attributes) { {} }
-    
-    it { is_expected.to be_valid }
+    context 'dateが正しく入力された時' do
+      let(:attributes) { {} }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'dateが入力されなかった時' do
+      let(:attributes) { { end_at: "" } }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context 'dateの値が不正な時' do
+      let(:attributes) { { end_at: "aaa" } }
+
+      it { is_expected.to be_invalid }
+    end
   end
 end

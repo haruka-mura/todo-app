034996e1d7033fb8f2221a0ba20b13db44ccdcc6
haruka-mura <muramoto.haruka@mwed.co.jp>
2018-06-26 15:16:14 +0900

    WIP

---
+++ spec/factories/task_due_date_factory.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :task_due_date do
+    end_at '2018-06-25 08:37:11'
+  end
+end
--- spec/models/task_due_date_spec.rb
+++ spec/models/task_due_date_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe TaskDueDate, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#valid?' do
+    subject { build :task_due_date, attributes }
+
+    let(:attributes) { {} }
+    
+    it { is_expected.to be_valid }
+  end
 end

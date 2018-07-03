ee83c95757b11aa0138e58b1da6c834246e122d0
haruka-mura <muramoto.haruka@mwed.co.jp>
2018-07-03 16:48:45 +0900

    publicじゃなくていいメソッドをprivateメソッドにする

--- app/models/task_form.rb
+++ app/models/task_form.rb
@@ -15,20 +15,6 @@ class TaskForm
     @task ||= Task.new(params)
   end
 
-  def new_or_update_or_destroy_task_due_date(end_at)
-    return task.task_due_date&.destroy if end_at.empty?
-    t_date = TaskDueDate.find_or_initialize_by(task_id: task.id)
-    t_date.end_at = end_at
-    task.task_due_date = t_date
-  end
-
-  def new_or_update_or_destroy_completion_date(begin_at)
-    return task.completion_date&.destroy if begin_at.empty?
-    c_date = CompletionDate.find_or_initialize_by(task_id: task.id)
-    c_date.begin_at = begin_at
-    task.completion_date = c_date
-  end
-
   private
 
     def params
@@ -39,5 +25,19 @@ class TaskForm
         team_id: team_id,
         state: state
       }
+    end
+
+    def new_or_update_or_destroy_task_due_date(end_at)
+      return task.task_due_date&.destroy if end_at.empty?
+      t_date = TaskDueDate.find_or_initialize_by(task_id: task.id)
+      t_date.end_at = end_at
+      task.task_due_date = t_date
+    end
+
+    def new_or_update_or_destroy_completion_date(begin_at)
+      return task.completion_date&.destroy if begin_at.empty?
+      c_date = CompletionDate.find_or_initialize_by(task_id: task.id)
+      c_date.begin_at = begin_at
+      task.completion_date = c_date
     end
 end
